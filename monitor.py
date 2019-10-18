4463b403bb3cc74b190d64eda2f917b15deaf6c6
Your Name <you@example.com>
2019-10-18 09:26:31 -0700

    monitor.py finished!

--- monitor.py
+++ monitor.py
@@ -3,26 +3,59 @@ import subprocess
 import os
 
 
-
 def usage():
-	print("usage: python monitor.py PYTHON_FILE [OPTIONS]")
+	print("\nusage:\n python monitor.py PYTHON_FILE [OPTIONS]\n")
+	raise SystemExit
 
 
-if __name__=="__main__":
-	if len(sys.argv)<2:
+def run_process(full_command):
+	command = full_command.split()
+	output = subprocess.check_output(command, stderr=subprocess.STDOUT)
+	print(output.decode("utf-8"))
+
+
+def get_python_files():
+	all_files = []
+	files = []
+	for (dirpath, dirnames, filenames) in os.walk("."):
+		all_files.extend(filenames)
+		break  #Collect only the files in the current dir
+	for f in all_files:
+		if f[-3:] == ".py":
+			files.append(f)
+	return files
+
+
+def inotify_wait():
+	python_files = get_python_files()
+	command = "inotifywait -e modify "
+	for f in python_files:
+		command = command + f + " "
+	print(command)
+	run_process(command)
+
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
 		usage()
-	else:
+	#run yapf on all python files in current directory
+	while True:
 		try:
-			f=[]
-			for (dirpath, dirnames, filenames) in os.walk("."):
-				f.extend(filenames)
-				break #Collect only the files in the current dir
-			print(f)
-
-			output = subprocess.check_output(["yapf","--style.yapf style.yapf -i *.py"],stderr=subprocess.STDOUT)
-			print(output.decode("utf-8"))
+			files = get_python_files()
+			for f in files:
+				command = "yapf --style style.yapf -i " + f
+				run_process(command)
 		except subprocess.CalledProcessError as e:
 			print("Error")
 			print(e.output.decode("utf-8"))
-
-
+			if input("Error running yapf.  Proceed?(y)") != "y":
+				continue
+		command = "python "
+		for arg in sys.argv[1:]:
+			command += arg
+		print(command)
+		run_process(command)
+		try:
+			inotify_wait()
+		except subprocess.CalledProcessError as e:
+			print(e.output.decode("utf-8"))
